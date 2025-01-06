be3e57f59a15d595023c976d2c122741ce94f857
Leonardo Kruss <leo.kruss@gmail.com>
2025-01-06 20:18:13 -0300

    Inserido primeiras modificações no layout inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import './index.css';
 
 function App() {
   return (
-    'Comming soon...'
-  );
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">X</span>
+          </li>
+        </ol>
+      </div>
+
+    </main>
+  )
 }
 
 export default App

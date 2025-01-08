19463ed05e404794cd67507f2bd5dcaadd9cb20c
Leonardo Kruss <leo.kruss@gmail.com>
2025-01-08 14:29:57 -0300

    Alterada lógica de manipulação dos estados do jogador, objetivo é reduzir ao máximo a quantidade de estados sem afetar a aplicação.

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import Player from './components/Player/Player';
 import './index.css';
 import Log from './components/Log/Log';
 
+function derivedActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [ gameTurns, setGameTurns ] = useState([]);
-  const [ activePlayer, setActivePlayer ] = useState('X');
+  // const [ activePlayer, setActivePlayer ] = useState('X');
+
+  const activePlayer = derivedActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
-
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
+      const currentPlayer = derivedActivePlayer(prevTurns);
 
       const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, 
         player:  currentPlayer }, 

f2f4b8f6b84bfc7e371743edc4d95f7740d326ef
Leonardo Kruss <leo.kruss@gmail.com>
2025-01-08 23:46:23 -0300

    Adicionado lógica adicional para resetar o tabuleiro do jogo.

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
 
   const activePlayer = derivedActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -68,6 +68,11 @@ function App() {
         return updatedTurns;
       });
   }
+
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -75,8 +80,10 @@ function App() {
           <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'}/>
           <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'}/>
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner}/>}
-        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns}/>
     </main>
